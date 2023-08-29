6650c6eb34ce44da2c391bbd6f572163d20d804e
Dan O'Donovan <dan.odonovan@gmail.com>
2023-08-29 11:36:18 +0100

    ci: add setup.py to checks

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 tidy:
-	isort lig3dlens tests --profile black
-	black lig3dlens tests
+	isort lig3dlens tests setup.py --profile black
+	black lig3dlens tests setup.py
 
 lint:
-	ruff check lig3dlens tests
-	isort lig3dlens tests --check-only --profile black
-	black --diff lig3dlens tests
+	ruff check lig3dlens tests setup.py
+	isort lig3dlens tests setup.py --check-only --profile black
+	black --diff lig3dlens tests setup.py
 
 test:
 	pytest -s --cov=lig3dlens --cov-report term-missing tests/
--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='lig3dlens',
-    version='0.0.1',
-    url='https://github.com/healx/lig3dlens.git',
-    # description='Open source ligand-based 3D VS toolbox',
+    name="lig3dlens",
+    version="0.0.1",
+    url="https://github.com/healx/lig3dlens.git",
+    description="Open source ligand-based 3D VS toolbox",
     packages=find_packages(include=["lig3dlens", "lig3dlens.*"]),
-    entry_points={
-        "console_scripts": [
-            "lig3dlens = lig3dlens.main:main"
-        ]
-    }
+    entry_points={"console_scripts": ["lig3dlens = lig3dlens.main:main"]}
     # install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
 )
