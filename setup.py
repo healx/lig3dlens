8e0e8525639818b110c0389db7f6ad769987e387
Dan O'Donovan <dan.odonovan@gmail.com>
2023-09-12 23:13:24 +0100

    refactor: lig3dlens-cluster entrypoint

--- setup.py
+++ setup.py
@@ -6,8 +6,11 @@ setup(
     url="https://github.com/healx/lig3dlens.git",
     description="Open source ligand-based 3D VS toolbox",
     packages=find_packages(include=["lig3dlens", "lig3dlens.*"]),
-    entry_points={"console_scripts": [
-        "lig3dlens-prepare = lig3dlens.prep_cmpd_library:main",
-        "lig3dlens-align = lig3dlens.main:main"
-    ]}
+    entry_points={
+        "console_scripts": [
+            "lig3dlens-prepare = lig3dlens.prep_cmpd_library:main",
+            "lig3dlens-align = lig3dlens.main:main",
+            "lig3dlens-cluster = lig3dlens.kmeans_clustering:main",
+        ]
+    },
 )
